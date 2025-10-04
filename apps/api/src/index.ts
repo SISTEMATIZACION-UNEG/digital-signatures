148216f268e322621244844f4bd88003634ab8ce
Alvaro Resplandor <alvarojrr79@gmail.com>
2025-10-04 11:58:31 -0400

    chore: setup hono api

---
+++ apps/api/src/index.ts
@@ -0,0 +1,20 @@
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+
+const app = new Hono();
+
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
+
+serve(
+  {
+    fetch: app.fetch,
+    port: 3000,
+  },
+  (info) => {
+    console.log(`Running on ${process.env.NODE_ENV} environment`);
+
+    console.log(`Server is running on http://localhost:${info.port}`);
+  }
+);
